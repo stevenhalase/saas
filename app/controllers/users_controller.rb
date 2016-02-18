511befb70ed2e73afaa88b96c82aef73efa82836
Steven Halase <steven.halase@gmail.com>
2016-02-18 07:12:52 +0000

    Added index action and updated link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
    
-    def show
-       @user = User.find( params[:id] )
-       
+    def index
+        
     end
     
+    def show
+       @user = User.find( params[:id] )
+    end
 end
